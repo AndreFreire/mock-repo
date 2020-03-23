af64898b92143286b71f3764535a2b244df3563f
Andre Freire <andre.freire@dasa.com.br>
2020-03-23 19:00:50 -0300

    m

--- hello/views.py
+++ hello/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework import status
-from rest_framework.response import Response
 
 
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return Response({},status=status.HTTP_202_ACCEPTED)
+    return HttpResponse(status=204)
 
 
 def db(request):
