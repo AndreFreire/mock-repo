2662428eb837963f43ee9036d831e89fc7fd0552
Andre Freire <andre.freire@dasa.com.br>
2020-03-23 18:05:53 -0300

    m

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.response import Response
+
 
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    return Response(content, status=status.HTTP_404_NOT_FOUND)
 
 
 def db(request):
