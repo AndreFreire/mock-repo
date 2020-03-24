935e76d826e27f7f7c7e0e118a111025a81dca06
Andre Freire <andre.freire@dasa.com.br>
2020-03-24 17:34:11 -0300

    m

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from time import sleep
 
 from .models import Greeting
 
 # Create your views here.
 @csrf_exempt
 def index(request):
+	#sleep(1000000000000)
     return HttpResponse(status=403)
 
 
