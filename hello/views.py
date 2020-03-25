bd0a8af73ad948ca692f51e09d16c1a3dd40e99c
Wesley Soares <wesley.soares.ext@dasa.com.br>
2020-03-25 12:19:03 -0300

    status 404

--- hello/views.py
+++ hello/views.py
@@ -9,7 +9,7 @@ from .models import Greeting
 @csrf_exempt
 def index(request):
     #sleep(1000000000000)
-    return HttpResponse(status=500)
+    return HttpResponse(status=404)
 
 
 def db(request):
